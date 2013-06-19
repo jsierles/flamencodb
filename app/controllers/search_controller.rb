ecc506309fb495c8e256dc9e9bcb65ffbacd8ed1
Joshua Sierles <joshua@diluvia.net>
2013-06-19 17:15:47 +0200

    improved search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   
   def search
+    @artists = Artist.basic_search(params[:q])
+    @albums = Album.basic_search(params[:q])
     @lyrics = Lyric.basic_search(params[:q])
+    @tracks = Track.basic_search(params[:q])
+    
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def occurrences_for(lyric)
+    lyric.lyric_occurences.collect do |lo|
+      "#{link_to lo.track.singer.name, artist_path(lo.track.singer)} por <i>#{lo.track.style}</i> en #{link_to lo.track.title, lo.track} #{audio_for(lo.track)}"  
+    end.join("<br />")
+  end
+
+  def audio_for(track)
+    if track.has_audio?
+      "<audio style=\"margin-top: 5px\" controls>
+        <source src=\"#{track.audio_url}\" type=\"audio/mpeg\">
+      Your browser does not support the audio element.
+      </audio>"
+    end
+  end
 end
