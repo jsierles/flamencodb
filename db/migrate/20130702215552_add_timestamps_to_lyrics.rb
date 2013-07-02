c659a72e3ffbe16f78310363fa143fe0d0451a33
Joshua Sierles <joshua@diluvia.net>
2013-07-03 00:10:16 +0200

    fix timestamps on lyrics

--- db/migrate/20130702215552_add_timestamps_to_lyrics.rb
+++ db/migrate/20130702215552_add_timestamps_to_lyrics.rb
@@ -2,5 +2,11 @@ class AddTimestampsToLyrics < ActiveRecord::Migration
   def change
     add_column :lyrics, :updated_at, :datetime
     add_column :lyrics, :created_at, :datetime
+    Lyric.find_each do |l|
+      now = Time.now
+      l.updated_at = now
+      l.created_at = now
+      l.save
+    end
   end
 end
