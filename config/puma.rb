7cef52e9dcc99430223415fe7845fc05a7c3fcec
Joshua Sierles <joshua@diluvia.net>
2013-07-14 20:54:34 +0200

    fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,25 +1,23 @@
 #!/usr/bin/env puma
 
-application_path = '/home/app/letrasflamencas'
+application_path = '/home/app/letrasflamencas/current'
+
+rackup "#{application_path}/config.ru"
 
 directory application_path
 
-environment :production
+environment 'production'
 
 daemonize true
 
-pidfile "#{application_path}/shared/tmp/pids/puma.pid"
+pidfile "#{application_path}/tmp/pids/puma.pid"
 
-state_path "#{application_path}/shared/tmp/puma.state"
+state_path "#{application_path}/tmp/puma.state"
 
-stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log", true
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log", true
 
 threads 0, 16
 
-bind "/tmp/rails.sock"
+bind "unix:/tmp/rails.sock"
 
 workers 2
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
